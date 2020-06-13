4466b9c3e2b53c03e031dcd4fca159f84f4b75ce
github Shumai715 <dice_715@outlook.jp>
2020-06-14 01:11:47 +0900

    記事一覧にてカテゴリを一つのみ表示させるようにした

--- index.php
+++ index.php
@@ -29,7 +29,12 @@
             </li>
             <li>
               <div class="article-category">
-                <?php the_category(); ?>
+                <?php
+                  $category = get_the_category();
+                    if ( $category[0] ) {
+                      echo '<a href="' . get_category_link( $category[0]->term_id ) . '">' . $category[0]->cat_name . '</a>';
+                    }
+                  ?>
               </div>
             </li>
           </ul>
