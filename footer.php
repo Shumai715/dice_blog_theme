1979179f9c1189781ff8ea95c06886ec220cdc6e
github Shumai715 <dice_715@outlook.jp>
2020-06-13 22:39:06 +0900

    レスポンシブデザインを大幅変更。各リンクを静的へ。

--- footer.php
+++ footer.php
@@ -4,13 +4,13 @@
 
         <nav>
           <ul>
-            <li class="footer-li"><h5><a href="#">ブログTOPへ</a></h5></li>
-            <li class="footer-li"><h5><a href="#">プライバシーポリシー</a></h5></li>
-            <li class="footer-li"><h5><a href="#">お問い合わせ</a></h5></li>
+            <li class="footer-li"><h5><a href="https://dice-blog.com/">ブログTOPへ</a></h5></li>
+            <li class="footer-li"><h5><a href="https://dice-blog.com/%e3%83%97%e3%83%a9%e3%82%a4%e3%83%90%e3%82%b7%e3%83%bc%e3%83%9d%e3%83%aa%e3%82%b7%e3%83%bc">プライバシーポリシー</a></h5></li>
+            <li class="footer-li"><h5><a href="https://dice-blog.com/%e3%81%8a%e5%95%8f%e3%81%84%e5%90%88%e3%82%8f%e3%81%9b">お問い合わせ</a></h5></li>
           </ul>
         </nav>
         <div class="Copyright">
-          <p>Copyright © <?php bloginfo('name'); ?> All Rights Reserved.</p>
+          <p>Copyright ©ダイス🎲ブログ All Rights Reserved.</p>
         </div>
       </div>
 
--- header.php
+++ header.php
@@ -17,17 +17,18 @@
 
     <!-- ▼ ヘッダー開始 ▼ -->
     <header class="top-header">
+      <div class="icon">  </div>
       <div class="header-h1-img">
         <a href="<?php echo esc_url( home_url('/')); ?>"><h1><span class="header-title">ダイス🎲ブログ</span></h1><p class="header-content">声優ラジオやゲーム、旅行が趣味のオタクの日記</p></a>
         <img class="top-img" src="<?php echo esc_url(get_theme_file_uri('images/top-img.png')); ?>" alt="<?php bloginfo('name'); ?>">
       </div>
         <nav>
          <ul class="top-nav">
-          <li class="top-item"><h2><a href="<?php echo esc_url( home_url('/')); ?>">HOME</a></h2></li>
-          <li class="top-item"><h2><a href="<?php echo esc_url( home_url('/category/ラジオ')); ?>">ラジオ</a></h2></li>
-          <li class="top-item"><h2><a href="<?php echo esc_url( home_url('/category/ゲーム')); ?>">ゲーム</a></h2></li>
-          <li class="top-item"><h2><a href="<?php echo esc_url( home_url('/category/旅行記・お出かけ日記')); ?>">旅行記</a></h2></li>
-          <li class="top-item"><h2><a href="<?php echo esc_url( home_url('/category/ライブ・イベント')); ?>">ライブ・イベント</a></h2></li>
+           <li class="top-item"><a href="https://dice-blog.com/">HOME</a></li>
+           <li class="top-item"><a href="https://dice-blog.com/entry/category/%e3%83%a9%e3%82%b8%e3%82%aa">ラジオ</a></li>
+           <li class="top-item"><a href="https://dice-blog.com/entry/category/%e3%82%b2%e3%83%bc%e3%83%a0">ゲーム</a></li>
+           <li class="top-item"><a href="https://dice-blog.com/entry/category/%e6%97%85%e8%a1%8c%e8%a8%98%e3%83%bb%e3%81%8a%e5%87%ba%e3%81%8b%e3%81%91%e6%97%a5%e8%a8%98">旅行記</a></li>
+           <li class="top-item"><a href="https://dice-blog.com/entry/category/%e3%83%a9%e3%82%a4%e3%83%96%e3%83%bb%e3%82%a4%e3%83%99%e3%83%b3%e3%83%88">ライブ・イベント</a></li>
          </ul>
       </nav>
 
